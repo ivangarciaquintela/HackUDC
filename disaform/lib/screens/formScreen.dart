34678b42eb391e94186999f39f6fee3566ce35cf
Ivan García Quintela <ivan.garcia.quintela@udc.es>
2024-02-17 22:23:45 +0100

    [IVAN] Grupos en FormScreen

--- disaform/lib/screens/formScreen.dart
+++ disaform/lib/screens/formScreen.dart
@@ -1,6 +1,7 @@
 import 'package:disaform/controller/form_controller.dart';
 import 'package:disaform/controller/form_type_controller.dart';
 import 'package:disaform/models/formFieldSchema.dart';
+import 'package:disaform/models/formGroupSchema.dart';
 import 'package:disaform/models/formItem.dart';
 import 'package:disaform/models/formSchema.dart';
 import 'package:disaform/widgets/dynamicWidget.dart';
@@ -56,6 +57,7 @@ class _FormularioScreenState extends State<FormularioScreen> {
               return Text('Error: ${snapshot.error}');
             } else {
               _formSchema = snapshot.data!;
+              //return buildFormListView();
               return buildFormListView();
             }
           },
@@ -129,6 +131,88 @@ class _FormularioScreenState extends State<FormularioScreen> {
     return true;
   }
 
+
+Widget buildFormListView() {
+  // Creamos un mapa para agrupar los campos por su grupo
+  Map<String, List<FormFieldSchema>> groupedFields = {};
+  List<FormGroupSchema>? formgroupSchemas = _formSchema.formGroups;
+  
+  // Iteramos sobre cada campo del esquema para agruparlos
+  for (var field in _formSchema.formFields) {
+    // Verificamos si el grupo ya existe en el mapa, si no existe lo creamos
+    if (!groupedFields.containsKey(field.fieldGroup)) {
+      groupedFields[field.fieldGroup] = [];
+    }
+    // Añadimos el campo al grupo correspondiente
+    groupedFields[field.fieldGroup]!.add(field);
+  }
+
+  // Creamos una lista de widgets que contendrá los campos agrupados
+  List<Widget> widgets = [];
+
+  // Iteramos sobre los grupos y sus campos para generar los widgets correspondientes
+  groupedFields.forEach((groupId, fields) {
+    String groupName = '';
+    // Encuentra el nombre del grupo correspondiente
+    for (var groupSchema in formgroupSchemas!) {
+      if (groupSchema.groupId == groupId) {
+        groupName = groupSchema.groupName;
+        break;
+      }
+    }
+    
+    // Agregamos un encabezado para el grupo si existe
+    if (groupName.isNotEmpty) {
+      widgets.add(
+  Container(
+    color: Colors.grey.withOpacity(0.05), // Color gris con opacidad 0.5
+    child: Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Text(
+        groupName,
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.red[800],
+        ),
+      ),
+    ),
+  ),
+);
+    }
+    
+    // Agregamos los campos del grupo
+    widgets.addAll(fields.map((field) {
+      // Inicializa el valor por defecto en null o cualquier otro valor predeterminado adecuado
+      var defaultValue = null;
+
+      // Busca el valor correspondiente manualmente
+      for (var item in _formSchema.formFields) {
+        if (item.fieldId == field.fieldId) {
+          defaultValue = item.fieldDefaultValue;
+          break; // Salir del bucle una vez que se encuentra el valor
+        }
+      }
+      field.fieldDefaultValue = defaultValue;
+      return DynamicFormField(schema: field,
+      isVisible:
+              shouldShow(field), // Determina si el campo debe ser visible
+          onChanged: (value) {
+            //Así controlamos cuando se modifica un campo
+            setState(() {
+              _fieldValues[field.fieldId] = value;
+            });
+          },);
+    }));
+  });
+
+  // Retornamos un ListView que contiene todos los widgets generados
+  return ListView(
+    children: widgets,
+  );
+}
+
+/*formField
   Widget buildFormListView() {
     return ListView.builder(
       itemCount: _formSchema.formFields.length,
@@ -149,6 +233,8 @@ class _FormularioScreenState extends State<FormularioScreen> {
       },
     );
   }
+*/
+
 
   //Para dependencias de campos
   bool shouldShow(FormFieldSchema formField) {
